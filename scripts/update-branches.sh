840a038f4c0c5e3ed99effab56915f7923ae82b7
Zoltán Böszörményi <zboszor@gmail.com>
2024-08-13 12:36:07 +0200

    scripts/update-branches.sh: Pull up kirkstone and up to scarthgap
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- scripts/update-branches.sh
+++ scripts/update-branches.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-git checkout main
+git checkout scarthgap
 git push origin HEAD
 
-for i in kirkstone langdale mickledore nanbield scarthgap ; do
+for i in kirkstone langdale mickledore nanbield ; do
 	git checkout $i
-	git merge origin/main
+	git merge origin/scarthgap
 	git push origin HEAD
 done
 
-git checkout main
+git checkout scarthgap
