d19a7d2cc8e26ba86cbfc72844e7b8f7a6fe1c91
Zoltán Böszörményi <zboszor@gmail.com>
2025-09-09 18:07:07 +0200

    scripts/update-branches.sh: Update for walnascar
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- scripts/update-branches.sh
+++ scripts/update-branches.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-git checkout main
+git checkout walnascar
 git push origin HEAD
 
-for i in styhead walnascar whinlatter ; do
+for i in styhead ; do
 	git checkout $i
 	git merge origin/main
 	git push origin HEAD
 done
 
-git checkout main
+git checkout walnascar
